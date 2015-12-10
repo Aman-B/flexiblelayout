72d0ed0ede7ec0f1cc08a9ef72c8cf458681f016
Aman Bakshi <kingaman.bakshi4@gmail.com>
2015-12-11 03:43:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/aman/flexiblelayout/PageFragment.java
@@ -0,0 +1,43 @@
+package com.example.aman.flexiblelayout;
+
+import android.os.Bundle;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by aman on 11/12/15.
+ */
+// In this case, the fragment displays simple text based on the page
+public class PageFragment extends Fragment {
+    public static final String ARG_PAGE = "ARG_PAGE";
+
+    private int mPage;
+
+    public static PageFragment newInstance(int page) {
+        Bundle args = new Bundle();
+        args.putInt(ARG_PAGE, page);
+        PageFragment fragment = new PageFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mPage = getArguments().getInt(ARG_PAGE);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_page, container, false);
+
+        return view;
+    }
+}
